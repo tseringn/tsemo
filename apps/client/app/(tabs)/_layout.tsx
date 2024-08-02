871e61a6a5ef23e87d2cc975c20304fe7686ff5d
tser <thetseringnorbu@gmail.com>
2024-08-02 10:40:47 -0400

    two tabs

--- apps/client/app/(tabs)/_layout.tsx
+++ apps/client/app/(tabs)/_layout.tsx
@@ -24,6 +24,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "How to play",
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? "info" : "info"} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
